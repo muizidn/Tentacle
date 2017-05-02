26e379fc924f56a1b746ec0bccf9258695cd8dfe
Matt Diephouse <matt@diephouse.com>
2017-05-02 19:35:00 -0400

    Remove Repository.server
    It doesn’t serve any real purpose here—it’s only really used in
    `precondition`s.

--- Sources/Tentacle/Repository.swift
+++ Sources/Tentacle/Repository.swift
@@ -11,32 +11,22 @@ import Foundation
 
 /// A GitHub.com or GitHub Enterprise repository.
 public struct Repository: CustomStringConvertible {
-    public let server: Server
     public let owner: String
     public let name: String
     
-    public init(server: Server = .dotCom, owner: String, name: String) {
-        self.server = server
+    public init(owner: String, name: String) {
         self.owner = owner
         self.name = name
     }
     
-    /// The URL of the repository.
-    public var url: URL {
-        return server.url
-            .appendingPathComponent(owner)
-            .appendingPathComponent(name)
-    }
-    
     public var description: String {
-        return "\(url)"
+        return "\(owner)/\(name)"
     }
 }
 
 extension Repository: Hashable {
     public static func ==(lhs: Repository, rhs: Repository) -> Bool {
-        return lhs.server == rhs.server
-            && lhs.owner.caseInsensitiveCompare(rhs.owner) == .orderedSame
+        return lhs.owner.caseInsensitiveCompare(rhs.owner) == .orderedSame
             && lhs.name.caseInsensitiveCompare(rhs.name) == .orderedSame
     }
 
