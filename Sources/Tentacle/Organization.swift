44c482a5a459f1f470d8291bb29f7b3de79abfde
Matt Diephouse <matt@diephouse.com>
2017-05-19 07:31:28 -0400

    Add an Organization type

---
+++ Sources/Tentacle/Organization.swift
@@ -0,0 +1,33 @@
+//
+//  Organization.swift
+//  Tentacle
+//
+//  Created by Matt Diephouse on 5/19/17.
+//  Copyright © 2017 Matt Diephouse. All rights reserved.
+//
+
+import Foundation
+
+/// An organization on GitHub or GitHub Enterprise.
+public struct Organization: CustomStringConvertible {
+    /// The organization's name.
+    public let name: String
+    
+    public init(_ name: String) {
+        self.name = name
+    }
+    
+    public var description: String {
+        return name
+    }
+}
+
+extension Organization: Hashable {
+    public static func ==(lhs: Organization, rhs: Organization) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
+    public var hashValue: Int {
+        return name.hashValue
+    }
+}
