ae7e37f4417a4cc232878d87401fde39df51fbe9
David Snabel-Caunt <dcaunt@gmail.com>
2017-09-27 16:40:54 +0100

    Fix SwiftPM build under Swift 4.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Tentacle",
     dependencies: [
-        .Package(url: "https://github.com/thoughtbot/Argo.git", versions: Version(4, 1, 2)..<Version(4, .max, .max)),
-        .Package(url: "https://github.com/thoughtbot/Curry.git", majorVersion: 3),
         .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 2),
-    ]
+    ],
+    swiftLanguageVersions: [3, 4]
 )
